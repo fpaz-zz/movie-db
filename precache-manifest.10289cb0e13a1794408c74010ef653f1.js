8adb6b3cbbbd2ed916b3f09eb6a9686866d4445e
fpaz <fpaz@me.com>
2019-04-01 11:33:16 +1000

    Updates

---
+++ precache-manifest.10289cb0e13a1794408c74010ef653f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bb80631b382c7965b33",
+    "url": "/movie-db/static/js/runtime~main.5114c00d.js"
+  },
+  {
+    "revision": "7298499bc18bcc113d10",
+    "url": "/movie-db/static/js/main.c307ef2d.chunk.js"
+  },
+  {
+    "revision": "978c309613c5ed78bea7",
+    "url": "/movie-db/static/js/2.1bffb47e.chunk.js"
+  },
+  {
+    "revision": "7298499bc18bcc113d10",
+    "url": "/movie-db/static/css/main.613f08c3.chunk.css"
+  },
+  {
+    "revision": "978c309613c5ed78bea7",
+    "url": "/movie-db/static/css/2.43517064.chunk.css"
+  },
+  {
+    "revision": "e5a4bc0bd3b05ccfba2cd1a0533945f3",
+    "url": "/movie-db/index.html"
+  }
+];
