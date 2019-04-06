b7acd596c647d9b308c7bfa08ed170cef982e2af
fpaz <fpaz@me.com>
2019-04-07 09:07:29 +1000

    refactor wrappers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return this.props.children;
-  }
+import { configureStore } from './reducer/store';
+import MovieBrowser from './components/MovieBrowser';
+import MovieDetails from './components/MovieDetails';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.scss';
+
+function App() {
+  const initialState = {};
+  const store = configureStore(initialState);
+
+  return (
+    <Provider store={store}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/" component={MovieBrowser} />
+          <Route exact path="/movie/:id" component={MovieDetails} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
 }
-
-App.propTypes = {
-  children: PropTypes.node.isRequired
-};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import PageTransition from 'react-router-page-transition';
-
-import { configureStore } from './reducer/store';
 import App from './App';
-import MovieBrowser from './components/MovieBrowser';
-import MovieDetails from './components/MovieDetails';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './styles/index.scss';
-
-const initialState = {};
-const store = configureStore(initialState);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App>
-      <Router basename={process.env.PUBLIC_URL}>
-        <PageTransition>
-          <Switch>
-            <Route exact path="/" component={MovieBrowser} />
-            <Route exact path="/movie/:id" component={MovieDetails} />
-          </Switch>
-        </PageTransition>
-      </Router>
-    </App>
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
